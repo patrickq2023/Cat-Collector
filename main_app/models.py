ea7bd8da89524bef4f3a5ad0856b43b6d8f24c14
patrickq2023 <151511696+patrickq2023@users.noreply.github.com>
2024-02-18 22:48:48 +0000

    fully crudding finished

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Cat(models.Model):
@@ -7,4 +8,11 @@ class Cat(models.Model):
   description = models.TextField(max_length=250)
   age = models.IntegerField()
 
+  # Changing this instance method, does not impact the DB
+  #therefore no migration required
 
+  def __str__(self):
+   return f'{self.name} ({self.id})'
+  
+  def get_absolute_url(self):
+    return reverse('detail', kwargs={'cat_id': self.id})
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Cat
+
+# cats = [
+#   {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
+#   {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
+# ]
 
 
 # Create your views here.
@@ -9,6 +16,23 @@ def about(request):
   return render(request, 'about.html')
 
 def cats_index(request):
+  cats = Cat.objects.all()
   return render(request, 'cats/index.html', {
     'cats': cats
   })
+
+def cats_detail(request, cat_id):
+  cat = Cat.objects.get(id=cat_id)
+  return render(request, 'cats/detail.html', {'cat': cat })
+
+class CatCreate(CreateView):
+  model = Cat
+  fields = '__all__'
+
+class CatUpdate(UpdateView):
+  model = Cat
+  fields = ['breed', 'description', 'age']
+
+class CatDelete(DeleteView):
+  model = Cat
+  success_url = '/cats'
