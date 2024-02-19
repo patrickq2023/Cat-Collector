f0de8215744249380c829a289c082b840848e620
patrickq2023 <151511696+patrickq2023@users.noreply.github.com>
2024-02-19 19:44:56 +0000

    fully crudding cat

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,14 @@
+from django.contrib import admin
 from django.db import models
 from django.urls import reverse
+from datetime import date
+
+
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
 
 # Create your models here.
 class Cat(models.Model):
@@ -8,11 +17,34 @@ class Cat(models.Model):
   description = models.TextField(max_length=250)
   age = models.IntegerField()
 
-  # Changing this instance method, does not impact the DB
-  #therefore no migration required
+  def fed_for_today(self):
+      return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)  
+
+    # Changing this instance method, does not impact the DB
+    #therefore no migration required
 
   def __str__(self):
-   return f'{self.name} ({self.id})'
-  
+    return f'{self.name} ({self.id})'
+    
   def get_absolute_url(self):
-    return reverse('detail', kwargs={'cat_id': self.id})
+      return reverse('detail', kwargs={'cat_id': self.id})
+    
+class Feeding(models.Model):
+  date = models.DateField('feeding date')
+  meal = models.CharField(
+    max_length=1,
+    # add the 'choices' field option
+    choices=MEALS,
+    # set the default value for meal to be 'B'
+    default=MEALS[0][0]
+  )
+  cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
+
+  def __str__(self):
+    # Nice method for obtaining the friendly value of a Field.choice
+    return f"{self.get_meal_display()} on {self.date} eaten by {self.cat}"
+  
+  # change the default sort
+  class Meta:
+    ordering = ['-date']
+
--- main_app/views.py
+++ main_app/views.py
@@ -1,14 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Cat
+from .forms import FeedingForm
 
 # cats = [
 #   {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
 #   {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
 # ]
 
-
 # Create your views here.
+# Include an .html file extension - unlike when rendering EJS templates
+
 def home(request):
   return render(request, 'home.html')
 
@@ -17,13 +19,31 @@ def about(request):
 
 def cats_index(request):
   cats = Cat.objects.all()
+   
   return render(request, 'cats/index.html', {
     'cats': cats
   })
 
 def cats_detail(request, cat_id):
   cat = Cat.objects.get(id=cat_id)
-  return render(request, 'cats/detail.html', {'cat': cat })
+   # instantiate FeedingForm to be rendered in the template 
+  feeding_form = FeedingForm()
+  return render(request, 'cats/detail.html', {
+    'cat': cat,
+    'feeding_form': feeding_form
+    })
+
+def add_feeding(request, cat_id):
+    # create a ModelForm instance using the data in request.POST
+    form = FeedingForm(request.POST)
+
+    if form.is_valid():
+    # don't save the form to the db until it
+    # has the cat_id assigned
+      new_feeding = form.save(commit=False)
+      new_feeding.cat_id = cat_id
+      new_feeding.save()
+    return redirect('detail', cat_id=cat_id)
 
 class CatCreate(CreateView):
   model = Cat
